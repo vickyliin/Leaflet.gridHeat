06d86d9986ba7e8cf1fbdc6cb60d27dd578a92f3
Lin <LinVickywr@ad.garmin.com>
2017-09-21 15:46:15 +0800

    change configs

---
+++ demo/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const root = path.resolve(__dirname, '../')
+
+module.exports = {
+  entry: ['babel-polyfill', 'index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+      __dirname
+    ]
+  },
+  devServer: {
+    port: 16888
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: path.resolve(root, 'node_modules/leaflet/dist/leaflet.css') },
+      { from: path.resolve(__dirname, 'index.html') }
+    ])
+  ]
+}
